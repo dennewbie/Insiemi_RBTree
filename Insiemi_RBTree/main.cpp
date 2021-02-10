aa7f5c9bd35cf918ab909604c9171de4b275cf4e
DC <caruso.denny@gmail.com>
2021-02-10 21:11:10 +0100

    Initial Commit

---
+++ Insiemi_RBTree/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Insiemi_RBTree
+//
+//  Created by Denny Caruso on 10/02/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

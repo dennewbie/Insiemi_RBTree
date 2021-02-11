8e7a28bf3317cd366b1b1ec3601e62fde34e69dc
DC <caruso.denny@gmail.com>
2021-02-11 11:11:10 +0100

    bugfix in man

--- Insiemi_RBTree/main.cpp
+++ Insiemi_RBTree/main.cpp
@@ -15,15 +15,15 @@ int main() {
     
     temp_1->setLeft(temp_2);
     temp_2->setLeft(temp_3);
-    int height = temp_1->calculateHeight();
-    int minHeight = temp_1->calculateMinHeight();
-    std::cout << "HEIGHT: " << height << "\n";
-    std::cout << "MIN_HEIGHT: " << minHeight << "\n";
-    
-    
-    std::cout << "HEIGHT temp2: " << temp_2->getHeight() << "\n";
-    std::cout << "MIN_HEIGHT temp2: " << temp_2->getMinHeight() << "\n";
-    std::cout << "HEIGHT temp3: " << temp_3->getHeight() << "\n";
-    std::cout << "MIN_HEIGHT temp3: " << temp_3->getMinHeight() << "\n";
+//    int height = temp_1->calculateHeight();
+//    int minHeight = temp_1->calculateMinHeight();
+//    std::cout << "HEIGHT: " << height << "\n";
+//    std::cout << "MIN_HEIGHT: " << minHeight << "\n";
+//    
+//    
+//    std::cout << "HEIGHT temp2: " << temp_2->getHeight() << "\n";
+//    std::cout << "MIN_HEIGHT temp2: " << temp_2->getMinHeight() << "\n";
+//    std::cout << "HEIGHT temp3: " << temp_3->getHeight() << "\n";
+//    std::cout << "MIN_HEIGHT temp3: " << temp_3->getMinHeight() << "\n";
     return 0;
 }
